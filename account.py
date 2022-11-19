c7a41ff10737d5031f86c840782b25ee17dcc009
Madeline Mewmews <maddie@mewmews.gay>
2022-11-18 22:17:34 -0600

    pytest tests, docstrings, type hinting

--- account.py
+++ account.py
@@ -1,24 +1,46 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name) -> None:
+        """
+        Initialization function for Account class
+        :param name: account name
+        """
         self.__account_name = f"{name}"
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount) -> bool:
+        '''
+        Function to deposit money into account
+        :param amount: Amount to add to account balance
+        :return: Success boolean
+        '''
         if (amount <= 0):
             return False
         else:
             self.__account_balance += amount
             return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount) -> bool:
+        '''
+        Function to withdraw money from account
+        :param amount: Amount to subtract to account balance
+        :return: Success boolean
+        '''
         if (amount <= 0) or (amount > self.__account_balance):
             return False
         else:
             self.__account_balance -= amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        Method to return account balance value
+        :return: Balance value guaranteed as float
+        """
         return float(self.__account_balance)
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        Method to return the account name as string
+        :return: Account name value
+        """
         return self.__account_name
---
+++ test_account.py
@@ -0,0 +1,33 @@
+from account import *
+
+
+class Test:
+    def setup_method(self):
+        self.p1 = Account("Dave Strider")
+        self.p2 = Account("John Egbert")
+
+    def test_init(self):
+        assert self.p1.get_name() == "Dave Strider"
+        assert self.p2.get_name() == "John Egbert"
+
+    def test_deposit(self):
+        assert self.p1.deposit(427) is True
+        assert self.p1.get_balance() == 427.0
+        assert self.p2.deposit(413) is True
+        assert self.p2.get_balance() == 413.0
+
+        assert self.p1.deposit(-345) is False
+        assert self.p2.deposit(0) is False
+
+    def test_withdraw(self):
+        # persist deposits from previous test
+        self.test_deposit()
+
+        assert self.p1.withdraw(64) is True
+        assert self.p1.get_balance() == 363.0
+        assert self.p2.withdraw(80) is True
+        assert self.p2.get_balance() == 333.0
+
+        assert self.p1.withdraw(428) is False
+        assert self.p2.withdraw(0) is False
+        assert self.p1.withdraw(-3) is False
