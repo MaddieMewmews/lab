a86c1163acd4a44c9ebc879e3b4758e5ca58aaa4
Madeline Mewmews <maddie@mewmews.gay>
2022-11-18 21:29:06 -0600

    update

--- account.py
+++ account.py
@@ -4,14 +4,14 @@ class Account:
         self.__account_balance = 0
 
     def deposit(self, amount):
-        if (amount <= 0) or (amount > self.__account_balance):
+        if (amount <= 0):
             return False
         else:
             self.__account_balance += amount
             return True
 
     def withdraw(self, amount):
-        if amount <= 0:
+        if (amount <= 0) or (amount > self.__account_balance):
             return False
         else:
             self.__account_balance -= amount
